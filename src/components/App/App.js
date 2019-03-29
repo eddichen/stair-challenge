18fae57c05d1076214a6ff5a586c18080ef97f01
eddichen <echen@inviqa.com>
2019-03-29 16:26:58 +0000

    adding eslint

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,110 +1,108 @@
 import React, { Component } from 'react';
-import { firebaseApp } from '../../base';
+import PropTypes from 'prop-types';
 import { Route } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableRow from '@material-ui/core/TableRow';
 import Button from '@material-ui/core/Button';
 import Avatar from '@material-ui/core/Avatar';
-import Header from "../Header";
+import { firebaseApp } from '../../base';
+import Header from '../Header';
 import './App.css';
 
 const styles = () => ({
   container: {
     paddingTop: 100
   }
-})
+});
 
 class App extends Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       users: null,
       currentMonth: null
-    }
+    };
+  }
+
+  componentDidMount() {
+    this.getCurrentMonth();
+    this.getUserData();
   }
 
   getUserData() {
     const db = firebaseApp.firestore();
-    let userDetails = []
+    const userDetails = [];
 
-    db.collection("users").get().then((querySnapshot) => {
-      querySnapshot.forEach(doc => {
-        userDetails.push(doc.data())
-      })
-
-      this.orderUsers(userDetails)
-    })
-  }
-
-  orderUsers(userDetails) {
-    const date = new Date()
-    const year = date.getFullYear()
-    const month = ("0" + (date.getMonth() + 1)).slice(-2)
-    const currentMonth = `${year}-${month}`
-    let usersRanked = userDetails;
-    
-    //adding an easily accessible monthly total to the user object
-    usersRanked.forEach(user => {
-      if(user.climbTotals[currentMonth] !== undefined) {
-        user.monthlyTotal = user.climbTotals[currentMonth]
-      }
-    })
-
-    //sorting the user in descending order according to climbTotals
-    usersRanked.sort((a, b) => {
-      return b.monthlyTotal - a.monthlyTotal
-    })
-
-    this.setState({
-      users: usersRanked
-    })
+    db.collection('users').get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        userDetails.push(doc.data());
+      });
+      this.orderUsers(userDetails);
+    });
   }
 
   getCurrentMonth() {
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    const currentMonth = new Date().getMonth()
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const currentMonth = new Date().getMonth();
 
     this.setState({
       currentMonth: months[currentMonth]
-    })
+    });
   }
 
-  progressWidth(userTotal=0) {
-    const highestRankTotal = this.state.users[0].monthlyTotal
-    return { width: `${userTotal/highestRankTotal * 100}%` }
-  } 
+  orderUsers(userDetails) {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = (`0${(date.getMonth() + 1)}`).slice(-2);
+    const currentMonth = `${year}-${month}`;
+    const usersRanked = userDetails;
 
-  componentDidMount() {
-    this.getCurrentMonth()
-    this.getUserData()
+    // adding an easily accessible monthly total to the user object
+    usersRanked.forEach((user) => {
+      if (user.climbTotals[currentMonth] !== undefined) {
+        user.monthlyTotal = user.climbTotals[currentMonth];
+      }
+    });
+
+    // sorting the user in descending order according to climbTotals
+    usersRanked.sort((a, b) => b.monthlyTotal - a.monthlyTotal);
+
+    this.setState({
+      users: usersRanked
+    });
+  }
+
+  progressWidth(userTotal = 0) {
+    const state = this.state;
+    const highestRankTotal = state.users[0].monthlyTotal;
+    return { width: `${userTotal / highestRankTotal * 100}%` };
   }
 
   render() {
-    const {classes} = this.props;
+    const { classes } = this.props;
 
     return (
       <div>
         <Header title="Stair Challenge" {...this.props} />
         <Grid container className={classes.container} spacing={16} justify="center">
           <Grid item xs={12}>
-            <Typography variant="h4">Leaderboard for {this.state.currentMonth}</Typography>
+            <Typography variant="h4">
+              Leaderboard for
+              {this.state.currentMonth}
+            </Typography>
           </Grid>
           {this.state.users !== null ? (
             <Grid item xs={12} md={6}>
-              {this.state.users.map((user, index) => (
-                <Grid container key={index} spacing={16} alignItems="center">
+              {this.state.users.map(user => (
+                <Grid container key={user.uid} spacing={16} alignItems="center">
                   <Grid item xs={1}>
                     <Avatar alt={`${user.name}`} src={user.avatar} />
                   </Grid>
                   <Grid item xs={9}>
                     <div className="progress">
-                      <span style={this.progressWidth(user.monthlyTotal)}></span>
+                      <span style={this.progressWidth(user.monthlyTotal)} />
                     </div>
                     <Typography variant="body1">{user.name}</Typography>
                   </Grid>
@@ -115,16 +113,18 @@ class App extends Component {
                 </Grid>
               ))}
             </Grid>
-          ): null}
+          ) : null}
           <Grid item xs={12}>
-            <Route render={({history}) => (          
-              <Button variant="contained" color="primary" onClick={() => {history.push('/stair-form')}}>Log your stair climb</Button>
-            )} />
+            <Route render={({ history }) => (<Button variant="contained" color="primary" onClick={() => { history.push('/stair-form'); }}>Log your stair climb</Button>)} />
           </Grid>
         </Grid>
       </div>
-    )
+    );
   }
 }
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired
+};
 
 export default withStyles(styles)(App);
--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import App from './components/App/App';
-import StairForm from "./components/StairForm";
-import UserDashboard from "./components/UserDashboard";
+import StairForm from './components/StairForm';
+import UserDashboard from './components/UserDashboard';
 
 const router = (
   <Router>
     <div>
-      <Route exact path="/" component={App}/>
+      <Route exact path="/" component={App} />
       <Route path="/stair-form" component={StairForm} />
       <Route path="/user-dashboard" component={UserDashboard} />
     </div>
   </Router>
-)
+);
 
 ReactDOM.render(router, document.getElementById('root'));
 
